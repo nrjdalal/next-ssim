ed5e721189838b24088446df2f48a57c62984085
Neeraj Dalal <admin@nrjdalal.com>
2021-06-23 18:57:48 +0530

    210623-185748

--- components/home/Recruiters.js
+++ components/home/Recruiters.js
@@ -4,6 +4,10 @@ const Recruiters = () => {
   return (
     <>
       <section className="relative h-[300px] overflow-hidden mt-12">
+        <div className="absolute z-10 h-[300px] w-screen flex">
+          <div className="h-[300px] w-1/2 bg-blue-500 opacity-20"></div>
+          <div className="hidden md:block h-[300px] w-1/2 bg-red-500 opacity-20"></div>
+        </div>
         <div className="flex animate-x">
           <img className="h-[300px] object-cover " src="https://raw.githubusercontent.com/nrjdalal/ns-ssim/master/brands.webp" alt="" />
           <img className="h-[300px] object-cover" src="https://raw.githubusercontent.com/nrjdalal/ns-ssim/master/brands.webp" alt="" />
@@ -17,7 +21,6 @@ const Recruiters = () => {
           0% {
             transform: translate3d(0%, 0, 0);
           }
-
           100% {
             transform: translate3d(-100%, 0, 0);
           }
