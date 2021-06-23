eba07bea1a638f2e49ee2ec34d6fe7ac9e1dc637
Neeraj Dalal <admin@nrjdalal.com>
2021-06-23 15:04:08 +0530

    210623-150408

--- components/Admission.js
+++ components/Admission.js
@@ -1,7 +1,7 @@
 const Admission = () => {
   return (
     <>
-      <div className="fixed top-1/2 -right-14 -rotate-90 z-50 bg-yellow-500 px-3 py-1 rounded text-white  cursor-pointer">Admission Open 2021</div>
+      <div className="fixed top-1/2 -right-16 -rotate-90 z-50 bg-yellow-500 px-3 py-1 rounded text-white  cursor-pointer">Admission Open 2021</div>
     </>
   )
 }
