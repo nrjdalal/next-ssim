da1810719f0330cf2e0e09ff6b245c06a26178de
Neeraj Dalal <admin@nrjdalal.com>
2021-06-23 13:12:23 +0530

    210623-131223

--- components/Header.js
+++ components/Header.js
@@ -62,7 +62,12 @@ const Header = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
               </svg>
             </a>
-            <a className="hover:text-gray-900">Life @ SSIM</a>
+            <a className="mr-5 hover:text-gray-900 flex items-center">
+              Placement
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+              </svg>
+            </a>
             <a className="ml-5 hover:text-gray-900">Contact Us</a>
           </nav>
           <div className="flex">
