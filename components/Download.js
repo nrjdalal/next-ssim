e369b45a9c3ac8e6e55c26f435137a40b3a2454c
abhishek garg <abhishek.garg@namasys.co>
2021-06-23 20:32:09 +0530

    210623-203209

--- components/Download.js
+++ components/Download.js
@@ -1,9 +1,11 @@
 const Download = () => {
   return (
     <>
-      <button className="fixed top-1/2 -left-16 rotate-90 z-50 bg-yellow-500 px-3 py-1 rounded text-white cursor-pointer" download="public/documents/SSIM Information Brochure 2021.pdf">
-        Download Brochure
-      </button>
+      <a>
+        <button className="fixed top-1/2 -left-16 rotate-90 z-50 bg-yellow-500 px-3 py-1 rounded text-white cursor-pointer" download="public/documents/SSIM Information Brochure 2021.pdf">
+          Download Brochure
+        </button>
+      </a>
     </>
   )
 }
