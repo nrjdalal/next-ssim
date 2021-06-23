aa80125298e8c16e53c9363a5c87a3e32917826d
Neeraj Dalal <admin@nrjdalal.com>
2021-06-23 15:41:42 +0530

    210623-154142

--- components/Download.js
+++ components/Download.js
@@ -1,7 +1,9 @@
 const Download = () => {
   return (
     <>
-      <button className="fixed top-1/2 -left-16 rotate-90 z-50 bg-yellow-500 px-3 py-1 rounded text-white cursor-pointer">Download Brochure</button>
+      <button className="fixed top-1/2 -left-16 rotate-90 z-50 bg-yellow-500 px-3 py-1 rounded text-white cursor-pointer" download="public/documents/SSIM Information Brochure 2021.pdf">
+        Download Brochure
+      </button>
     </>
   )
 }
