fbe7caea8a8b10e41e5ca6567ea8f5dd1659866b
abhishek garg <abhishek.garg@namasys.co>
2021-06-23 20:52:26 +0530

    210623-205226

--- components/Download.js
+++ components/Download.js
@@ -1,7 +1,7 @@
 const Download = () => {
   return (
     <>
-      <a src="/documents/brochure.pdf">
+      <a href="/documents/brochure.pdf" download>
         <button className="fixed top-1/2 -left-16 rotate-90 z-50 bg-yellow-500 px-3 py-1 rounded text-white cursor-pointer" download="">
           Download Brochure
         </button>
