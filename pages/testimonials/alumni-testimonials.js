e8355207113b50444b367b673a6356c360facf1a
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 03:04:21 +0530

    added all empty pages

---
+++ pages/testimonials/alumni-testimonials.js
@@ -0,0 +1,11 @@
+const alumni_testimonials = () => {
+  return (
+    <>
+      <div className="w-screen h-[720px] flex items-center justify-center prose">
+        <blockquote>Content Coming Soon</blockquote>
+      </div>
+    </>
+  )
+}
+
+export default alumni_testimonials
