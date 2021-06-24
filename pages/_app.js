03687989335b7c4cc212222107e44e113faa02e1
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 02:44:33 +0530

    testing admission pop-up

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/globals.css'
 // Global Imports
 import Header from '../components/Header'
 import Admission from '../components/Admission'
+import Admission_form from '../components/Admission-form'
 import Download from '../components/Download'
 import Footer from '../components/Footer'
 
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Admission />
+      <Admission_form />
       <Download />
       <Header />
       <Component {...pageProps} />
