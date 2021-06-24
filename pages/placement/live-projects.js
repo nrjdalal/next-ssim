89fc33c83687a15155f83f874802740b7ac5e836
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 03:06:24 +0530

    v3 baby

--- pages/placement/live-projects.js
+++ pages/placement/live-projects.js
@@ -2,7 +2,7 @@ const rename_later = () => {
   return (
     <>
       <div className="w-screen h-[720px] flex items-center justify-center prose">
-        <blockquote>Content Coming Soo</blockquote>
+        <blockquote>Content Coming Soon</blockquote>
       </div>
     </>
   )
---
+++ pages/placement/placement-overview.js
@@ -0,0 +1,11 @@
+const rename_later = () => {
+  return (
+    <>
+      <div className="w-screen h-[720px] flex items-center justify-center prose">
+        <blockquote>Content Coming Soon</blockquote>
+      </div>
+    </>
+  )
+}
+
+export default rename_later
