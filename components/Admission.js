991eb5a4ed1c9e063959a55b3be8288309d06c81
Neeraj Dalal <admin@nrjdalal.com>
2021-06-24 17:00:34 +0530

    moved addmission button to rightmost

--- components/Admission.js
+++ components/Admission.js
@@ -1,7 +1,7 @@
 const Admission = () => {
   return (
     <>
-      <button className="fixed top-1/2 -right-16 -rotate-90 z-50 bg-green-500 px-3 py-1 rounded text-white  cursor-pointer">Admission Open 2021</button>
+      <button className="fixed top-1/2 right-[-72px] -rotate-90 z-50 bg-green-500 px-3 py-1 rounded text-white  cursor-pointer">Admission Open 2021</button>
     </>
   )
 }
