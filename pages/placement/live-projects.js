1d1d7b2ed37c8b2ab7b75c9bfbffe0b2bdf11a44
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 03:05:27 +0530

    mighty push

---
+++ pages/placement/live-projects.js
@@ -0,0 +1,11 @@
+const rename_later = () => {
+  return (
+    <>
+      <div className="w-screen h-[720px] flex items-center justify-center prose">
+        <blockquote>Content Coming Soo</blockquote>
+      </div>
+    </>
+  )
+}
+
+export default rename_later
