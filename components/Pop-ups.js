38f7f3d16cdeeb5fa94509cccb2abc5f715f1078
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 08:16:04 +0530

    shifted admission popup and download popup to pop_ups.js

--- components/Pop-ups.js
+++ components/Pop-ups.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Admission = () => {
+const Pop_ups = () => {
   const [isActive, setActive] = useState(true)
   const toggleClass = () => {
     setActive(!isActive)
@@ -11,12 +11,19 @@ const Admission = () => {
 
   return (
     <>
+      {/* Download Brochures */}
+      {/* <a style={isActive ? { display: 'none' } : { position: 'fixed' }} href="/documents/brochure.pdf" download>
+        <button className="fixed top-1/2 -left-16 rotate-90 z-40 bg-yellow-500 px-3 py-1 rounded text-white cursor-pointer" download="">
+          Download Brochure
+        </button>
+      </a> */}
+      {/* Admission Open Button */}
       <button onClick={toggleClass} style={isActive ? { display: 'none' } : { position: 'fixed' }} className="fixed top-1/2 right-[-72px] -rotate-90 z-40 bg-green-500 px-3 py-1 rounded text-white  cursor-pointer">
         Admission Open 2021
       </button>
       {/* Pop Up */}
       <div className="w-5/6 md:w-[500px] z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-50 rounded-3xl text-white border-2 border-gray-500 cursor-pointer" style={isActive ? { position: 'fixed' } : { display: 'none' }}>
-        <div className="prose my-12 px-5 flex flex-col text-center">
+        <div className="prose mt-4 mb-12 px-5 flex flex-col text-center">
           <div onClick={closeMenu} className="flex justify-end">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
@@ -45,4 +52,4 @@ const Input_mandtory = (prop) => {
   )
 }
 
-export default Admission
+export default Pop_ups
--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,14 @@ import '../styles/globals.css'
 
 // Global Imports
 import Header from '../components/Header'
-import Admission from '../components/Admission'
-import Admission_form from '../components/Admission-form'
-import Download from '../components/Download'
+import Pop_ups from '../components/Pop-ups'
 import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Admission />
-      <Admission_form />
-      <Download />
       <Header />
+      <Pop_ups />
       <Component {...pageProps} />
       <Footer />
     </>
