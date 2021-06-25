b0a1aed2c59981f27ba22d25bf2e61bfb2166164
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 20:23:41 +0530

    wtf is happening

--- components/Universal.js
+++ components/Universal.js
@@ -29,3 +29,35 @@ export const ProseWide = (props) => {
     </div>
   )
 }
+
+export const ContentOnLeft = (props) => {
+  return (
+    <FlexColumnCenter className="lg:flex-row">
+      <img
+        className={`order-1 lg:order-2 rounded-3xl h-[300px] w-full max-w-[400px] object-cover object-center ${props.className}`}
+        src={props.src}
+        alt={props.alt}
+      />
+      <div className="order-2 lg:order-1 w-full lg:pr-12">
+        <h2 className="text-center">{props.title}</h2>
+        <blockquote>{props.children}</blockquote>
+      </div>
+    </FlexColumnCenter>
+  )
+}
+
+export const ContentOnRight = (props) => {
+  return (
+    <FlexColumnCenter className="lg:flex-row">
+      <img
+        className={`rounded-3xl h-[300px] w-full max-w-[400px] object-cover object-center ${props.className}`}
+        src={props.src}
+        alt={props.alt}
+      />
+      <div className="w-full lg:pl-12">
+        <h2 className="text-center">{props.title}</h2>
+        <blockquote>{props.children}</blockquote>
+      </div>
+    </FlexColumnCenter>
+  )
+}
--- pages/about/the-institute.js
+++ pages/about/the-institute.js
@@ -1,4 +1,9 @@
-import { FlexColumnCenter, ProseWide } from '../../components/Universal'
+import {
+  ContentOnLeft,
+  ContentOnRight,
+  FlexColumnCenter,
+  ProseWide,
+} from '../../components/Universal'
 
 const the_institute = () => {
   return (
@@ -6,124 +11,107 @@ const the_institute = () => {
       <ProseWide className="px-6 py-12 text-justify">
         <FlexColumnCenter>
           <img
-            className="rounded-3xl h-[400px] w-[600px] object-cover object-center"
+            className="rounded-3xl h-[400px] w-full max-w-[600px] object-cover object-center"
             src="https://ssimdwarka.org/wp-content/uploads/2019/12/7.jpeg"
             alt=""
           />
+          <blockquote>
+            Sri Sukhmani Institute of Management is a premier Institute, set up
+            under the Sri Sukhmani Group of Institutions. The Group was
+            established more than 3 decades ago, in 1979, by
+            Philanthropist-Educationist Prof. Avtar Singh and has since carved a
+            niche for itself in the area of Management.
+            <br />
+            <br />
+            At SSIM, we equip our students with knowledge, skills and confidence
+            that are relevant to contemporary managerial practices. The
+            institute strives to sharpen Management skills and produce managers
+            of tomorrow who can meet the global demands in the field of
+            management.
+            <br />
+            <br />
+            Sri Sukhmani Institute of Management has a beautiful campus, fully
+            equipped with labs, library, AC classrooms, Wi-fi accessibility and
+            other state-of-the-art infrastructure. We offer PGDM courses with
+            specialization in Human Resources, Marketing, Finance, and IT &
+            Operations. We focus on providing a cordial environment for the
+            students to learn, experience, and experiment, covering all facets
+            of professional education.
+            <br />
+            <br />
+            Our campus is located in Dwarka, Delhi and has state of the art
+            infrastructure with all the facilities essential for conducive
+            learning. It is easily accessible by road, rail or air with the
+            airport being a mere 6 km away.
+          </blockquote>
         </FlexColumnCenter>
-        <blockquote>
-          Sri Sukhmani Institute of Management is a premier Institute, set up
-          under the Sri Sukhmani Group of Institutions. The Group was
-          established more than 3 decades ago, in 1979, by
-          Philanthropist-Educationist Prof. Avtar Singh and has since carved a
-          niche for itself in the area of Management.
-          <br />
-          <br />
-          At SSIM, we equip our students with knowledge, skills and confidence
-          that are relevant to contemporary managerial practices. The institute
-          strives to sharpen Management skills and produce managers of tomorrow
-          who can meet the global demands in the field of management.
-          <br />
-          <br />
-          Sri Sukhmani Institute of Management has a beautiful campus, fully
-          equipped with labs, library, AC classrooms, Wi-fi accessibility and
-          other state-of-the-art infrastructure. We offer PGDM courses with
-          specialization in Human Resources, Marketing, Finance, and IT &
-          Operations. We focus on providing a cordial environment for the
-          students to learn, experience, and experiment, covering all facets of
-          professional education.
-          <br />
-          <br />
-          Our campus is located in Dwarka, Delhi and has state of the art
-          infrastructure with all the facilities essential for conducive
-          learning. It is easily accessible by road, rail or air with the
-          airport being a mere 6 km away.
-        </blockquote>
 
-        <h1 className="text-center italic">Our Campus</h1>
+        <h1 className="pt-12 text-center italic">Our Campus</h1>
 
-        <img
-          className="rounded-3xl h-[400px] w-full object-cover object-center"
+        <ContentOnRight
+          title="Classroom"
           src="https://ssimdwarka.org/wp-content/uploads/2019/12/1.jpg"
-          alt=""
-        />
-        <h2 className="text-center italic">Classroom</h2>
-        <blockquote>
+        >
           The classrooms are aesthetically designed to provide a stimulating
           atmosphere, conducive to the all-round development of the students.
           The classrooms are fully air-conditioned and equipped with the latest
           facilities for presentations, with the modern audio-visual system, LCD
           projectors, computers, and the internet.
-        </blockquote>
+        </ContentOnRight>
 
-        <img
-          className="rounded-3xl h-[400px] w-full object-cover object-center"
+        <ContentOnLeft
+          title="IT Lab"
           src="https://ssimdwarka.org/wp-content/uploads/2019/12/2.jpg"
-          alt=""
-        />
-        <h2 className="text-center italic">IT Lab</h2>
-        <blockquote>
+        >
           The SSIM Institute has a well-equipped state of the art computer lab,
           with well-equipped systems of the latest technical configuration. The
           required legal softwares are also installed in each system. High-speed
           internet connectivity provides extended access to vast intellectual
           resources.
-        </blockquote>
+        </ContentOnLeft>
 
-        <img
-          className="rounded-3xl h-[400px] w-full object-cover object-center"
+        <ContentOnRight
+          title="Library"
           src="https://ssimdwarka.org/wp-content/uploads/2019/12/3.jpg"
-          alt=""
-        />
-        <h2 className="text-center italic">Library</h2>
-        <blockquote>
+        >
           SSIM Dwarka has a well-stocked library, replete with the latest and
           most sought after books, National Journals, International Journals &
           e-journals. We realize that the library and information systems play a
           pivotal role in the learning activities of students. Accordingly, we
           ensure that our library is kept updated with the latest information
           and learning tools.
-        </blockquote>
+        </ContentOnRight>
 
-        <img
-          className="rounded-3xl h-[400px] w-full object-cover object-center"
+        <ContentOnLeft
+          title="Co-curricular Activities"
           src="https://ssimdwarka.org/wp-content/uploads/2019/12/4.jpg"
-          alt=""
-        />
-        <h2 className="text-center italic">Co-curricular Activities</h2>
-        <blockquote>
+        >
           While you study at SSIM Dwarka, you will be actively encouraged to
           participate & engage in co-curricular activities. The institute
           organizes many social and sporting events throughout the year to
           develop holistic creativity.
-        </blockquote>
+        </ContentOnLeft>
 
-        <img
-          className="rounded-3xl h-[400px] w-full object-cover object-center"
+        <ContentOnRight
+          title="Cafeteria"
           src="https://ssimdwarka.org/wp-content/uploads/2019/12/5.jpg"
-          alt=""
-        />
-        <h2 className="text-center italic">Cafeteria</h2>
-        <blockquote>
+        >
           The institution has an ultra-modern canteen with an aesthetic sense of
           design and décor which offers hygienic, wholesome and nutritious food
           at subsidized rates to satisfy different taste buds. Complete, planned
           food is served in neat, clean & hygienic surroundings.
-        </blockquote>
+        </ContentOnRight>
 
-        <img
-          className="rounded-3xl h-[400px] w-full object-cover object-center"
+        <ContentOnLeft
+          title="Wi-Fi Enabled"
           src="https://ssimdwarka.org/wp-content/uploads/2019/12/6.jpg"
-          alt=""
-        />
-        <h2 className="text-center italic">Wi-Fi Enabled</h2>
-        <blockquote>
+        >
           We have a State-of-the-Art, fully air-conditioned, Wifi campus with
           24x7 high bandwidth internet connectivity. The IT lab consists of
           computers in interconnected local area networks. Our IT programs are
           regularly updated by incorporating the latest hardware/ software
           developments across the world.
-        </blockquote>
+        </ContentOnLeft>
       </ProseWide>
     </>
   )
