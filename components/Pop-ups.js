7aded1e7192fd9ac1cdbe3c123a22cf7b783ba41
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 20:25:05 +0530

    set pop up to false for development

--- components/Pop-ups.js
+++ components/Pop-ups.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import axios from 'axios'
 
 const Pop_ups = () => {
-  const [isActive, setActive] = useState(true)
+  const [isActive, setActive] = useState(false)
 
   const toggleClass = () => {
     setActive(!isActive)
@@ -22,31 +22,63 @@ const Pop_ups = () => {
     <>
       {/* Download Brochures */}
       <a href="/documents/brochure.pdf" download>
-        <button style={isActive ? { display: 'none' } : { position: 'fixed' }} className="top-1/2 -left-16 rotate-90 z-30 bg-yellow-500 px-3 py-1 rounded text-white cursor-pointer" download="">
+        <button
+          style={isActive ? { display: 'none' } : { position: 'fixed' }}
+          className="top-1/2 -left-16 rotate-90 z-30 bg-yellow-500 px-3 py-1 rounded text-white cursor-pointer"
+          download=""
+        >
           Download Brochure
         </button>
       </a>
       {/* Admission Open Button */}
-      <button onClick={toggleClass} style={isActive ? { display: 'none' } : { position: 'fixed' }} className="top-1/2 right-[-72px] -rotate-90 z-30 bg-green-500 px-3 py-1 rounded text-white  cursor-pointer">
+      <button
+        onClick={toggleClass}
+        style={isActive ? { display: 'none' } : { position: 'fixed' }}
+        className="top-1/2 right-[-72px] -rotate-90 z-30 bg-green-500 px-3 py-1 rounded text-white  cursor-pointer"
+      >
         Admission Open 2021
       </button>
       {/* Pop Up */}
-      <div style={isActive ? { position: 'fixed' } : { display: 'none' }} className="h-full w-full bg-black top-0 z-40 bg-opacity-50"></div>
-      <div className="w-11/12 max-w-[500px] z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-50 rounded-3xl text-white border-2 border-gray-500 cursor-pointer" style={isActive ? { position: 'fixed' } : { display: 'none' }}>
+      <div
+        style={isActive ? { position: 'fixed' } : { display: 'none' }}
+        className="h-full w-full bg-black top-0 z-40 bg-opacity-50"
+      ></div>
+      <div
+        className="w-11/12 max-w-[500px] z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-50 rounded-3xl text-white border-2 border-gray-500 cursor-pointer"
+        style={isActive ? { position: 'fixed' } : { display: 'none' }}
+      >
         <div className="prose mt-4 mb-12 px-5 flex flex-col text-center">
           <div onClick={closeMenu} className="flex justify-end">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10 text-yellow-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           </div>
-          <form onSubmit={(e) => handleSubmit(e)} action="https://formsubmit.co/bahipe7012@seatto.com" method="POST">
+          <form
+            onSubmit={(e) => handleSubmit(e)}
+            action="https://formsubmit.co/bahipe7012@seatto.com"
+            method="POST"
+          >
             <h3 className="text-center pb-5">Admission Open 2021-22</h3>
             <Input_mandtory name="Full Name" type="text" />
             <Input_mandtory name="Mobile Number" type="text" />
             <Input_mandtory name="Email Address" type="email" />
             <Input_mandtory name="City" type="text" />
             <input type="hidden" name="_captcha" value="false" />
-            <button type="submit" className="mt-10 w-full bg-gray-500 border-0 py-3 px-3 focus:outline-none hover:bg-yellow-600 rounded text-sm text-white">
+            <button
+              type="submit"
+              className="mt-10 w-full bg-gray-500 border-0 py-3 px-3 focus:outline-none hover:bg-yellow-600 rounded text-sm text-white"
+            >
               SUMBIT
             </button>
           </form>
