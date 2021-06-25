d86c0bffc6ec7d758257da2b0b7226975c992a57
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 09:02:08 +0530

    footer activated

--- components/Footer.js
+++ components/Footer.js
@@ -1,75 +1,48 @@
+import Link from 'next/link'
+
 const Footer = () => {
   return (
     <>
       <footer className="text-gray-600 body-font bg-blue-900">
         <div className="container px-5 py-12 mx-auto">
           <div className="flex flex-wrap md:text-left text-center -mb-10 -mx-4 justify-between">
-            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-yellow-300 tracking-widest text-sm mb-3">ABOUT</h2>
-              <nav className="list-none mb-10">
-                <li>
-                  <a className="text-white hover:text-yellow-300">The Group</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">The Institution</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Newsletter</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Dean Message</a>
-                </li>
+            <div className="lg:w-1/6 md:w-1/2 w-full text-center">
+              <h2 className="font-medium text-yellow-300 tracking-widest text-sm mb-3">ABOUT</h2>
+              <nav className="mb-10 flex flex-col">
+                <Footer_link name="The Group" to="/about/the-group" />
+                <Footer_link name="The Institute" to="/about/the-institute" />
+                <Footer_link name="Vision Mission" to="/about/vision-mission" />
+                <Footer_link name="Chairman's Message" to="/about/chairmans-message" />
+                <Footer_link name="Dean's Message" to="/about/deans-message" />
+                <Footer_link name="Board of Governors" to="/about/board-of-governors" />
               </nav>
             </div>
-            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-yellow-300 tracking-widest text-sm mb-3">PROGRAM</h2>
-              <nav className="list-none mb-10">
-                <li>
-                  <a className="text-white hover:text-yellow-300">Jr. Management</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Sr. Management</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Upcoming Events</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Aluminies</a>
-                </li>
+            <div className="lg:w-1/6 md:w-1/2 w-full text-center">
+              <h2 className="font-medium text-yellow-300 tracking-widest text-sm mb-3">PROGRAM</h2>
+              <nav className="mb-10 flex flex-col">
+                <Footer_link name="Programme" to="/pgdm/programme" />
+                <Footer_link name="Specializations" to="/pgdm/specializations" />
+                <Footer_link name="Value Added Courses" to="/pgdm/value-added-courses" />
+                <Footer_link name="FAQs" to="/pgdm/faqs" />
+                <Footer_link name="Placement Overview" to="/placement/placement-overview" />
+                <Footer_link name="Our Recruiters" to="/placemnet/our-recruiters" />
               </nav>
             </div>
-            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-yellow-300 tracking-widest text-sm mb-3">ADMISSIONS</h2>
-              <nav className="list-none mb-10">
-                <li>
-                  <a className="text-white hover:text-yellow-300">Online Form</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Fee Structure</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Payment Options</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Brochure</a>
-                </li>
+            <div className="lg:w-1/6 md:w-1/2 w-full text-center">
+              <h2 className="font-medium text-yellow-300 tracking-widest text-sm mb-3">ADMISSIONS</h2>
+              <nav className="mb-10 flex flex-col">
+                <Footer_link name="Admission Process" to="/admissions/admission-process" />
+                <Footer_link name="Fee Structure" to="/admissions/fee-structure" />
+                <Footer_link name="Education Loan" to="/admissions/education-loan" />
+                <Footer_link name="Online Form" to="/admissions/online-form" />
+                <Footer_link name="Download Brochure" to="/admissions/download-brochure" />
+                <Footer_link name="FAQs" to="/admissions/faqs" />
               </nav>
             </div>
-            <div className="lg:w-1/6 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-yellow-300 tracking-widest text-sm mb-3">OTHERS</h2>
-              <nav className="list-none mb-10">
-                <li>
-                  <a className="text-white hover:text-yellow-300">Sitemap</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Disclaimer</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Privacy</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-yellow-300">Terms</a>
-                </li>
+            <div className="lg:w-1/6 md:w-1/2 w-full text-center">
+              <h2 className="font-medium text-yellow-300 tracking-widest text-sm mb-3">OTHERS</h2>
+              <nav className="mb-10 flex flex-col">
+                <Footer_link name="" to="/" />
               </nav>
             </div>
           </div>
@@ -82,6 +55,17 @@ const Footer = () => {
           </div>
         </div>
       </footer>
+    </>
+  )
+}
+
+const Footer_link = (prop) => {
+  // Usage: <Footer_link name="" to="/" />
+  return (
+    <>
+      <Link href={prop.to}>
+        <a className="text-white hover:text-yellow-300 pb-1">{prop.name}</a>
+      </Link>
     </>
   )
 }
