bb22f0b4a8e1b84e0004def91399416bde603993
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 21:20:20 +0530

    created breadcrumb

---
+++ components/Breadcrumb.js
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+
+const Breadcrumb = (props) => {
+  return (
+    <>
+      <div className="relative h-[100px] sm:h-[150px] md:h-[200px] w-full bg-gray-800 flex items-center">
+        <img
+          className="h-full opacity-50"
+          src={props.src || 'https://source.unsplash.com/1920x200'}
+          alt=""
+        />
+        <div className="absolute px-[20px] sm:px-[40px] md:px-[60px] lg:px-[80px] xl:px-[100px] 2xl:px-[120px]">
+          <p className="text-xl sm:text-2xl text-green-500 tracking-widest">
+            <Link href="/">
+              <a className="">Home</a>
+            </Link>
+            &nbsp;&nbsp;
+            {' > '}
+            &nbsp;&nbsp;
+            <a className="">{props.title}</a>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Breadcrumb
--- pages/about/the-group.js
+++ pages/about/the-group.js
@@ -1,9 +1,9 @@
+import Breadcrumb from '../../components/Breadcrumb'
+
 const rename_later = () => {
   return (
     <>
-      <div className="w-screen h-[720px] flex items-center justify-center prose">
-        <blockquote>Content Coming Soon</blockquote>
-      </div>
+      <Breadcrumb title="The Group" />
     </>
   )
 }
