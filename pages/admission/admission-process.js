58023f5ff84c141c34ad4cbdad945fc51f786b21
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 17:46:10 +0530

    what is happening, call me

--- pages/admission/admission-process.js
+++ pages/admission/admission-process.js
@@ -2,7 +2,7 @@ const rename_later = () => {
   return (
     <>
       <div className="w-screen h-[720px] flex items-center justify-center prose">
-        <blockquote>Content Coming Soon</blockquote>
+        <blockquote>Content Coming Sooner?</blockquote>
       </div>
     </>
   )
