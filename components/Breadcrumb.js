adff8fb570a5fc5e27acfe6bb13111d65b3b6a42
Neeraj Dalal <admin@nrjdalal.com>
2021-06-25 21:24:49 +0530

    added relative images in breadcrumb

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -6,7 +6,7 @@ const Breadcrumb = (props) => {
       <div className="relative h-[100px] sm:h-[150px] md:h-[200px] w-full bg-gray-800 flex items-center">
         <img
           className="h-full opacity-30"
-          src={props.src || 'https://source.unsplash.com/1920x200'}
+          src={props.src || 'https://source.unsplash.com/1920x200/?management'}
           alt=""
         />
         <div className="absolute px-[20px] sm:px-[40px] md:px-[60px] lg:px-[80px] xl:px-[100px] 2xl:px-[120px]">
