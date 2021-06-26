c27337e35753d4a8e011a1a714ed2b177b77c6a1
Neeraj Dalal <admin@nrjdalal.com>
2021-06-26 15:34:22 +0530

    feesssss

--- pages/admission/fee-structure.js
+++ pages/admission/fee-structure.js
@@ -1,8 +1,10 @@
+import { ProseWide } from '../../components/Universal'
+
 const chairmans_message = () => {
   return (
     <>
-      <div className="container mx-auto prose my-12 px-5 text-center">
-        <h2 className="italic">Fee Strucutre</h2>
+      <ProseWide className="container mx-auto prose my-12 px-5 text-center flex flex-col items-center">
+        {/* <h2 className="italic">Fee Strucutre</h2>
 
         <h4>Registration Fees: INR 50,000/-</h4>
         <li>At the time of Admission</li>
@@ -12,69 +14,77 @@ const chairmans_message = () => {
 
         <h4>Total Fee: INR 5,90,000/-</h4>
         <li>Inclusive of Laptop and International Study Tour</li>
-        <li>Exclusive of INR 5,000/- for Business Suit</li>
+        <li>Exclusive of INR 5,000/- for Business Suit</li> */}
 
+        <blockquote>
+          Tuition Fees: INR 5,90,000/- payable in four installments Registration
+          Fees at the time of Admission: INR 50,000/- Total Fee: INR 5,90,000/-
+          Inclusive of Laptop and International Study Tour (Exclusive of INR
+          5,000/- for Business Suit)
+        </blockquote>
         <br />
 
-        <table>
-          <thead>
-            <tr>
-              <th>
-                <strong>Installment</strong>
-              </th>
-              <th>
-                <strong>Type</strong>
-              </th>
-              <th>
-                <strong>Amount</strong>
-              </th>
-              <th>
-                <strong>Due Date</strong>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Registration</td>
-              <td>Admission</td>
-              <td>Rs.50,000</td>
-              <td>At the time of Registration</td>
-            </tr>
-            <tr>
-              <td>First</td>
-              <td>Tuition</td>
-              <td>Rs.1,50,000</td>
-              <td>Within 30 days of Registration</td>
-            </tr>
-            <tr>
-              <td>Second</td>
-              <td>Tuition</td>
-              <td>Rs.1,50,000</td>
-              <td>Before 15th December 2021</td>
-            </tr>
-            <tr>
-              <td>Third</td>
-              <td>Tuition</td>
-              <td>Rs.1,20,000</td>
-              <td>Before 15th June 2022</td>
-            </tr>
-            <tr>
-              <td>Fourth</td>
-              <td>Tuition</td>
-              <td>Rs.1,20,000</td>
-              <td>Before 15th November 2022</td>
-            </tr>
-            <tr>
-              <td>
-                <strong>Total</strong>
-              </td>
-              <td></td>
-              <td>
-                <strong>Rs.5,90,000</strong>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="p-5 border-2 border-gray-100 flex justify-center w-2/3 text-sm sm:text-base">
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  <strong>Installment</strong>
+                </th>
+                <th>
+                  <strong>Type</strong>
+                </th>
+                <th>
+                  <strong>Amount</strong>
+                </th>
+                <th>
+                  <strong>Due Date</strong>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Registration</td>
+                <td>Admission</td>
+                <td>Rs.50,000</td>
+                <td>At the time of Registration</td>
+              </tr>
+              <tr>
+                <td>First</td>
+                <td>Tuition</td>
+                <td>Rs.1,50,000</td>
+                <td>Within 30 days of Registration</td>
+              </tr>
+              <tr>
+                <td>Second</td>
+                <td>Tuition</td>
+                <td>Rs.1,50,000</td>
+                <td>Before 15th December 2021</td>
+              </tr>
+              <tr>
+                <td>Third</td>
+                <td>Tuition</td>
+                <td>Rs.1,20,000</td>
+                <td>Before 15th June 2022</td>
+              </tr>
+              <tr>
+                <td>Fourth</td>
+                <td>Tuition</td>
+                <td>Rs.1,20,000</td>
+                <td>Before 15th November 2022</td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Total</strong>
+                </td>
+                <td></td>
+                <td>
+                  <strong>Rs.5,90,000</strong>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
 
         <h2 className="italic">RTGS / NEFT / BANK TRANSFER</h2>
 
@@ -118,7 +128,7 @@ const chairmans_message = () => {
             </tr>
           </tbody>
         </table>
-      </div>
+      </ProseWide>
     </>
   )
 }
