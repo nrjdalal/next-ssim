da4d18b4062ece5cb651232edab210312f459cea
Neeraj Dalal <admin@nrjdalal.com>
2021-06-26 14:12:32 +0530

    recruiters

--- components/home/Recruiters.js
+++ components/home/Recruiters.js
@@ -3,6 +3,13 @@ import React from 'react'
 const Recruiters = () => {
   return (
     <>
+      <div className="relative">
+        <div className="absolute left-1/2 -translate-x-1/2 bottom-0 bg-white p-5 z-10 border-b-2 border-gray-100 rounded-tr-3xl rounded-tl-3xl">
+          <h1 className="text-2xl italic font-semibold text-[#096b97]">
+            Our Recruiters
+          </h1>
+        </div>
+      </div>
       <div className="container mx-auto flex flex-col items-center overflow-hidden">
         <section className="relative h-[150px] w-full sm:w-4/5 md:w-3/4 lg:2/3 xl:wd-1/2">
           <div className="animate-x">
