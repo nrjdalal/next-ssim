667dc6444cba8ac8b531156bafb218033f24e38a
Neeraj Dalal <admin@nrjdalal.com>
2021-06-26 12:32:47 +0530

    added admission process

--- pages/admission/admission-process.js
+++ pages/admission/admission-process.js
@@ -1,9 +1,68 @@
+import Breadcrumb from '../../components/Breadcrumb'
+import { ProseWide } from '../../components/Universal'
+
 const rename_later = () => {
   return (
     <>
-      <div className="w-screen h-[720px] flex items-center justify-center prose">
-        <blockquote>Content Coming Sooner?</blockquote>
-      </div>
+      <Breadcrumb title="admission process" />
+      <ProseWide className="pb-12">
+        <h2 className="pt-12 text-center">ADMISSION CRITERIA</h2>
+        <p>
+          A Bachelor&apos;s degree in any discipline with minimum 50% marks from
+          a recognized University. Candidates appearing for their final year
+          exam may also apply. Short listing will be based on CAT/MAT/CMAT and
+          S-SAT score and GD/PI/TI.
+        </p>
+        <h2 className="text-center">HOW TO APPLY</h2>
+        <p>
+          Forms are available on campus at: Sri Sukhmani Institute of Management
+          Plot No 2, Sector 8, Near Dwarka Sector 9 Metro Station, New Delhi -
+          110075 Phone: +91 8929399191, +91 8588890906 Email:
+          leads@ssimdelhi.org The applicants can also Apply Online or Download
+          form from the website http://www/ssimdelhi.org
+          <br />
+          <br />
+          Duly filled forms should be submitted at the institute office.
+          Documents are to be submitted along with Application Form. All the
+          forms must be supported by a Demand Draft of INR 550/- (fees can also
+          be paid through online mode)
+          <br />
+          <br />
+          The Demand Draft should be drawn in favour of &quot;Sri Sukhmani
+          Institute of Management&quot; payable at New Delhi, India.
+          <br />
+          <br />
+          The completed form should reach the office before the last date of
+          filling of ApplicationForms.
+        </p>
+        <h2 className="text-center">ADMISSION PROCESS AT CAMPUS</h2>
+        <p>
+          Qualifying candidates will be informed about the date, time and venue
+          of Admission Procedure (Written test and / or Group Discussion and /
+          or Personal/Telephonic Interview) via telephone and email. In certain
+          cases, options of dates will be available giving greater flexibility.
+          The Admission procedure may last till late in the evening. You are
+          requested to make traveling & boarding arrangements on your own.
+        </p>
+        <h2 className="text-center">OFFER OF ADMISSION</h2>
+        <p>
+          All selected candidates will get an intimation of their selection
+          through an Admission Letter. Details regarding payment of fees at the
+          time of admission, allotment to hostel and date, time and commencement
+          of the academic session will be mentioned in the admission letter.
+        </p>
+        <h2 className="text-center">ANTI-RAGGING MEASURES</h2>
+        <p>
+          Ragging in any form is banned at SSIM, New Delhi. It is a punishable
+          offence in accordance with one of the recent guidelines of the
+          Honourable Supreme Court of India. SSIM, New Delhi has a
+          ‘zero-tolerance policy’ on ragging. The institute will take
+          disciplinary action/legal proceeding/expulsion from the Institute, as
+          per the directives of the Supreme Court of India, if ant student(s)
+          is/are found guilty of ragging inside or outside the premises of the
+          Institute.
+        </p>
+      </ProseWide>
     </>
   )
 }
