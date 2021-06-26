866f13aea0c27b89263e5c068d774134dbcfc148
Neeraj Dalal <admin@nrjdalal.com>
2021-06-26 12:35:22 +0530

    added education loan

--- pages/admission/education-loan.js
+++ pages/admission/education-loan.js
@@ -1,9 +1,20 @@
+import Breadcrumb from '../../components/Breadcrumb'
+import { ProseWide } from '../../components/Universal'
+
 const rename_later = () => {
   return (
     <>
-      <div className="w-screen h-[720px] flex items-center justify-center prose">
-        <blockquote>Content Coming Soon</blockquote>
-      </div>
+      <Breadcrumb title="education loan" />
+      <ProseWide className="pb-12">
+        <blockquote className="pt-12">
+          Sri Sukhmani Institute of Management is recognized by all the
+          Nationalized and Private Banks for grant of education loans to its
+          students. The Institute Facilitate students with necessary documents
+          Admission Letter, Loan Letter, Bonafide Letter and AICTE approved
+          documents towards the processing of bank loans from nationalized and
+          private banks.
+        </blockquote>
+      </ProseWide>
     </>
   )
 }
