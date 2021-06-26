1bbe99ff949abd6b1d9c581087b1bb61c94b9730
Neeraj Dalal <admin@nrjdalal.com>
2021-06-26 09:32:09 +0530

    bkl kar kaam

--- pages/about/vision-mission.js
+++ pages/about/vision-mission.js
@@ -6,12 +6,12 @@ const rename_later = () => {
     <>
       <Breadcrumb title="the group" />
       <ProseWide className="pb-12">
-        <h2 className="pt-12 text-center italic">OUR VISION</h2>
+        <h2 className="pt-12 text-center">OUR VISION</h2>
         <blockquote>
           To be a world class school of management known for its innovative
           pedagogy, research and building professional competence.
         </blockquote>
-        <h2 className="pt-12 text-center italic">OUR MISSION</h2>
+        <h2 className="text-center">OUR MISSION</h2>
         <blockquote>
           To provide world-class management education in India in an atmosphere
           of freedom, while adhering to strict academic standards in research
