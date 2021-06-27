3e4847c9ca6017987c8e1b34d6f23ce2c19666cd
abhishek garg <abhishek.garg@namasys.co>
2021-06-28 01:05:22 +0530

    contact form

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -3,25 +3,27 @@ import { useState } from 'react'
 import axios from 'axios'
 
 const Contact_us = () => {
-  // const initialState = {
-  //   email:"",
-  //   message:"",
-  // }
+  const initialState = {
+    name:"",
+    email:"",
+    message:"",
+  }
 
-  // const [htmlFormState, sethtmlFormState] = useState(initialState);
+  const [FormState, setFormState] = useState(initialState);
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   axios.post("https://htmlFormsubmit.co/ajax/admin@nrjdalal.com", {...htmlFormState}).then(res=>{
-  //     if(res.status === 200){
-  //       setActive(false)
-  //       sethtmlFormState(initialState)
-  //       alert("htmlForm submitted")
-  //     }
-  //   }).catch(e => {
-  //     alert("htmlForm not submitted")
-  //   });
-  // }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("https://htmlFormsubmit.co/ajax/admin@nrjdalal.com", {...FormState}).then(res=>{
+      if(res.status === 200){
+        setActive(false)
+        setFormState(initialState)
+        alert("htmlForm submitted")
+      }
+    }).catch(e => {
+      alert("htmlForm not submitted")
+    });
+  }
+
 
   return (
     <>
@@ -35,7 +37,7 @@ const Contact_us = () => {
               marginHeight="0"
               marginWidth="0"
               scrolling="no"
-              src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3503.819103693248!2d77.06655081455824!3d28.575194293425604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1bd4a99cf95b%3A0x55b100fe5c566f41!2sSri%20Sukhmani%20Institute%20of%20Management%2C%20Delhi!5e0!3m2!1sen!2sin!4v1624821954769!5m2!1sen!2sin"
               width="100%"
               height="100%"
               frameBorder="0"
@@ -46,8 +48,7 @@ const Contact_us = () => {
                   ADDRESS
                 </h2>
                 <p className="mt-1">
-                  Photo booth tattooed prism, portland taiyaki hoodie neutra
-                  typewriter
+                  Plot No 2 Block-PSP, Sector 8 Dwarka, Dwarka, Delhi, 110075
                 </p>
               </div>
               <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -60,16 +61,16 @@ const Contact_us = () => {
                 <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
                   PHONE
                 </h2>
-                <p className="leading-relaxed">123-456-7890</p>
+                <p className="leading-relaxed">+91-9650709489</p>
               </div>
             </div>
           </div>
+          <form onSubmit={(e) => handleSubmit(e)}>
           <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-              Feedback
+              Contact Us
             </h2>
             <p className="leading-relaxed mb-5 text-gray-600">
-              Post-ironic portland shabby chic echo park, banjo fashion axe
             </p>
             <div className="relative mb-4">
               <label htmlFor="name" className="leading-7 text-sm text-gray-600">
@@ -79,6 +80,8 @@ const Contact_us = () => {
                 type="text"
                 id="name"
                 name="name"
+                value={FormState.name}
+                onChange={(e) => {setFormState({...FormState, name:e.target.value})}}
                 className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
@@ -93,6 +96,8 @@ const Contact_us = () => {
                 type="email"
                 id="email"
                 name="email"
+                value={FormState.email}
+                onChange={(e) => {setFormState({...FormState.email, email:e.target.value})}}
                 className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
@@ -106,17 +111,19 @@ const Contact_us = () => {
               <textarea
                 id="message"
                 name="message"
+                value={FormState.message}
+                onChange={(e) => {setFormState({...FormState, message:e.target.value})}}
                 className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              ></textarea>
+              />
             </div>
             <button className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
               Button
             </button>
             <p className="text-xs text-gray-500 mt-3">
-              Chicharrones blog helvetica normcore iceland tousled brook viral
-              artisan.
+              
             </p>
           </div>
+          </form>
         </div>
       </section>
     </>
