3e458a89bfe9df9aa6d4b2449192eeaf2b72cc18
Neeraj Dalal <admin@nrjdalal.com>
2021-06-28 02:28:08 +0530

    font push

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,12 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
 
         <body className="relative w-screen select-none overflow-x-hidden cursor-default">
