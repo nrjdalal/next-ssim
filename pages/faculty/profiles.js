a3b458f64319494f5b9cca6bb78cb49b2fd09ad1
Neeraj Dalal <admin@nrjdalal.com>
2021-06-27 20:36:03 +0530

    margin added

--- pages/faculty/profiles.js
+++ pages/faculty/profiles.js
@@ -4,7 +4,7 @@ const rename_later = () => {
   return (
     <>
       <Breadcrumb title="Faculty" />
-      <div className="container mx-auto py-24">
+      <div className="container mx-auto py-24 max-w-[1080px]">
         {/* Prof. Dr. Sujata Kaul */}
 
         <Profile
@@ -14,9 +14,12 @@ const rename_later = () => {
           degree="PhD, MA"
           years="28"
         >
-          Prof. Dr. Sujata Kaul obtained MA & PhD in Economics and has been a result-oriented professional having about 28 years of experience that includes over 20 years in academics as a Professor of Business Economics. Dr. Sujata Kaul is currently with SSIM as Dean - Academics.
+          Prof. Dr. Sujata Kaul obtained MA & PhD in Economics and has been a
+          result-oriented professional having about 28 years of experience that
+          includes over 20 years in academics as a Professor of Business
+          Economics. Dr. Sujata Kaul is currently with SSIM as Dean - Academics.
         </Profile>
-        
+
         <Profile
           name="Prof. Dr. Rachna Sharma"
           src="/images/faculty/Dr Rachna Sharma.png"
@@ -29,7 +32,7 @@ const rename_later = () => {
           than 18 years of experience in teaching HR domain and has industrial
           experience in Personnel Department in Multinational Companies.
         </Profile>
-        
+
         <Profile
           name="Prof. Dr. Anindita Sharma"
           src="/images/faculty/default-avatar.png"
@@ -50,7 +53,11 @@ const rename_later = () => {
           degree="PGDM, B.Com"
           years="14"
         >
-          Prof. Sushant Sinha is Associate Professor in marketing area. He is graduate in commerce from Avadh university and hold PGDM from IMT Ghaziabad. He has 14 years of rich corporate experience and specialization in strategic management. He is the Director – Student Welfare and Institutional Collaboration.
+          Prof. Sushant Sinha is Associate Professor in marketing area. He is
+          graduate in commerce from Avadh university and hold PGDM from IMT
+          Ghaziabad. He has 14 years of rich corporate experience and
+          specialization in strategic management. He is the Director – Student
+          Welfare and Institutional Collaboration.
         </Profile>
 
         <Profile
@@ -60,7 +67,11 @@ const rename_later = () => {
           degree="MBA, B.Sc (Maths)"
           years="10"
         >
-          Prof. Nitesh Srivastava is Asst. Professor in the International Business. He is graduate  in B.Sc (Maths) from Avadh University and holds a Master’s Degree in IB & Marketing, He has 10 Years of corporate experience and specialize in International Business & Foreign Trade
+          Prof. Nitesh Srivastava is Asst. Professor in the International
+          Business. He is graduate in B.Sc (Maths) from Avadh University and
+          holds a Master’s Degree in IB & Marketing, He has 10 Years of
+          corporate experience and specialize in International Business &
+          Foreign Trade
         </Profile>
 
         <Profile
@@ -70,7 +81,10 @@ const rename_later = () => {
           degree="Phd (Pursuing),M.Phill (Computer Science And Technology)"
           years="8"
         >
-          Mr. Irfan Amin is an IT professional with M.Phill from Himalayan University (Arunachal Pradesh), M.Sc (IT) from H.N.B Garwhal Central  university And B.sc (IT) From university of Kashmir. He has over 8 years of industry as well as academic experience.
+          Mr. Irfan Amin is an IT professional with M.Phill from Himalayan
+          University (Arunachal Pradesh), M.Sc (IT) from H.N.B Garwhal Central
+          university And B.sc (IT) From university of Kashmir. He has over 8
+          years of industry as well as academic experience.
         </Profile>
 
         <Profile
@@ -80,10 +94,15 @@ const rename_later = () => {
           degree="Phd (Pursuing),MBA (Finance & HR)"
           years="10"
         >
-          An emphatically motivated, self-starting, versatile and strongly committed to progressive and innovative methodologies. Has a sharp & analytical mind, ability to see end to end keeping organization goals in mind, ability to evaluate and effectively focus on right target, strong communication & intra personal skills.
-          She is a commerce graduate from HNB Garhwal Central University and MBA in Finance and HR from Uttrakhand Technical University.
+          An emphatically motivated, self-starting, versatile and strongly
+          committed to progressive and innovative methodologies. Has a sharp &
+          analytical mind, ability to see end to end keeping organization goals
+          in mind, ability to evaluate and effectively focus on right target,
+          strong communication & intra personal skills. She is a commerce
+          graduate from HNB Garhwal Central University and MBA in Finance and HR
+          from Uttrakhand Technical University.
         </Profile>
-        
+
         <Profile
           name="Ms Simran Kaur"
           src="\images\faculty\default-avatar.png"
@@ -91,9 +110,11 @@ const rename_later = () => {
           degree="MBA (Finance), B.Com"
           years="5"
         >
-          Ms. Simran is Asst. Professor in the Finance Domain. She is graduate  in Commerce and holds a Master’s Degree in Finance Specialization, She has 5 Years of corporate experience and specialize in Financial Trading, Advisory and Analytics.
+          Ms. Simran is Asst. Professor in the Finance Domain. She is graduate
+          in Commerce and holds a Master’s Degree in Finance Specialization, She
+          has 5 Years of corporate experience and specialize in Financial
+          Trading, Advisory and Analytics.
         </Profile>
-        
 
         {/* Other 1,2,3... */}
       </div>
