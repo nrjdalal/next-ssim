f5cb052190afc25857a6f0943bf128eb0ffb1999
abhishek garg <abhishek.garg@namasys.co>
2021-06-28 01:07:39 +0530

    textarea

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -114,10 +114,10 @@ const Contact_us = () => {
                 value={FormState.message}
                 onChange={(e) => {setFormState({...FormState, message:e.target.value})}}
                 className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              />
+              ></textarea>
             </div>
             <button className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
-              Button
+              SUBMIT
             </button>
             <p className="text-xs text-gray-500 mt-3">
               
