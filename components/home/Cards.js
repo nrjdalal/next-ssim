655d8c08090034440f7c23e800d4fc62441500e2
Neeraj Dalal <admin@nrjdalal.com>
2021-06-27 13:55:54 +0530

    rectified features

--- components/home/Cards.js
+++ components/home/Cards.js
@@ -1,71 +1,95 @@
 const Cards = () => {
   return (
     <>
-      <section className="text-gray-400 body-font">
+      <section className="text-gray-400 body-font text-justify">
         <div className="container px-5 py-6 pb-12 mx-auto">
           <div className="flex flex-wrap -m-4 justify-center">
             <div className="xl:w-1/4 md:w-1/2 p-4">
               <div className="bg-yellow-500 p-6 rounded-lg">
                 <img
-                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  className="h-[300px] rounded w-full object-cover object-top mb-6"
                   src="/images/other/chairman.jpeg"
                   alt="content"
                 />
 
                 <div className="bg-white px-2 rounded text-center">
-                  <h3 className="tracking-widest text-gray-900 text-lg font-semibold title-font">CHAIRMAN&apos;S MESSAGE</h3>
+                  <h3 className="tracking-widest text-gray-900 text-lg font-semibold title-font">
+                    CHAIRMAN&apos;S MESSAGE
+                  </h3>
                 </div>
 
-                <p className="pt-3 text-white text-sm text-justify">
-                SSIM students are professionals and intellectuals, in the making. The institution provides a learning environment that is adaptable and flexible, facilitating potential change in the higher education pattern. The learning environment is also used as a strategic tool for power and excellence. This is done with an objective to support the learning paradigm that provides education for the student in a manner most suited to them.
+                <p className="pt-3 text-white text-sm line-clamp-4">
+                  SSIM students are professionals and intellectuals, in the
+                  making. The institution provides a learning environment that
+                  is adaptable and flexible, facilitating potential change in
+                  the higher education pattern. The learning environment is also
+                  used as a strategic tool for power and excellence. This is
+                  done with an objective to support the learning paradigm that
+                  provides education for the student in a manner most suited to
+                  them.
                 </p>
               </div>
             </div>
             <div className="xl:w-1/4 md:w-1/2 p-4">
               <div className="bg-blue-500 p-6 rounded-lg">
                 <img
-                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  className="h-[300px] rounded w-full object-cover object-center mb-6"
                   src="https://images.unsplash.com/photo-1546422904-90eab23c3d7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1504&q=80"
                   alt="content"
                 />
                 <div className="bg-white px-2 rounded text-center">
-                  <h3 className="tracking-widest text-gray-900 text-lg font-semibold title-font">NEWS AND EVENTS</h3>
+                  <h3 className="tracking-widest text-gray-900 text-lg font-semibold title-font">
+                    NEWS AND EVENTS
+                  </h3>
                 </div>
-                <p className="pt-3 text-white text-sm">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate praesentium iure eveniet, nulla quae numquam repellat esse porro incidunt eos? Placeat praesentium perferendis, impedit deserunt consectetur consequatur. Culpa, beatae!
-                  Dicta!
+                <p className="pt-3 text-white text-sm line-clamp-4">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Cupiditate praesentium iure eveniet, nulla quae numquam
+                  repellat esse porro incidunt eos? Placeat praesentium
+                  perferendis, impedit deserunt consectetur consequatur. Culpa,
+                  beatae! Dicta!
                 </p>
               </div>
             </div>
             <div className="xl:w-1/4 md:w-1/2 p-4">
               <div className="bg-green-500 p-6 rounded-lg">
                 <img
-                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  className="h-[300px] rounded w-full object-cover object-center mb-6"
                   src="https://images.unsplash.com/photo-1567168539593-59673ababaae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
                   alt="content"
                 />
                 <div className="bg-white px-2 rounded text-center">
-                  <h3 className="tracking-widest text-gray-900 text-lg font-semibold title-font">ALUMNI TESTIMONIAL</h3>
+                  <h3 className="tracking-widest text-gray-900 text-lg font-semibold title-font">
+                    ALUMNI TESTIMONIAL
+                  </h3>
                 </div>
-                <p className="pt-3 text-white text-sm">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate praesentium iure eveniet, nulla quae numquam repellat esse porro incidunt eos? Placeat praesentium perferendis, impedit deserunt consectetur consequatur. Culpa, beatae!
-                  Dicta!
+                <p className="pt-3 text-white text-sm line-clamp-4">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Cupiditate praesentium iure eveniet, nulla quae numquam
+                  repellat esse porro incidunt eos? Placeat praesentium
+                  perferendis, impedit deserunt consectetur consequatur. Culpa,
+                  beatae! Dicta!
                 </p>
               </div>
             </div>
             <div className="xl:w-1/4 md:w-1/2 p-4">
               <div className="bg-red-500 p-6 rounded-lg">
                 <img
-                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  className="h-[300px] rounded w-full object-cover object-center mb-6"
                   src="https://images.unsplash.com/photo-1590415108862-fa060c0e358f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGZlZWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
                   alt="content"
                 />
                 <div className="bg-white px-2 rounded text-center">
-                  <h3 className="tracking-widest text-gray-900 text-lg font-semibold title-font">NEWSLETTER</h3>
+                  <h3 className="tracking-widest text-gray-900 text-lg font-semibold title-font">
+                    NEWSLETTER
+                  </h3>
                 </div>
-                <p className="pt-3 text-white text-sm">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate praesentium iure eveniet, nulla quae numquam repellat esse porro incidunt eos? Placeat praesentium perferendis, impedit deserunt consectetur consequatur. Culpa, beatae!
-                  Dicta!
+                <p className="pt-3 text-white text-sm line-clamp-4">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Cupiditate praesentium iure eveniet, nulla quae numquam
+                  repellat esse porro incidunt eos? Placeat praesentium
+                  perferendis, impedit deserunt consectetur consequatur. Culpa,
+                  beatae! Dicta!
                 </p>
               </div>
             </div>
