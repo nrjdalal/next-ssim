9c29cd90189e4b990e1b19abdf30110dddfe73b5
Neeraj Dalal <admin@nrjdalal.com>
2021-06-28 00:11:16 +0530

    idk idk

--- components/home/Logos.js
+++ components/home/Logos.js
@@ -7,13 +7,31 @@ const Logos = () => {
         <div className="container px-5 mx-auto">
           <div className="flex flex-col sm:flex-row justify-center items-center">
             <div className="relative px-2 py-6">
-              <img className="mw-33p pr300 px350-ns w-100p" height="60" width="60" src="/images/footerLogo/AICTE.png" />
+              <img
+                className="mw-33p pr300 px350-ns w-100p"
+                height="90"
+                width="90"
+                src="/images/footerLogo/AIMA.png"
+                alt=""
+              />
             </div>
             <div className="relative px-10 py-6">
-              <img className="mw-33p pr300 px350-ns w-100p" height="60" width="60" src="/images/footerLogo/AIMA.jpg" />
+              <img
+                className="mw-33p pr300 px350-ns w-100p"
+                height="90"
+                width="90"
+                src="/images/footerLogo/AICTE.jpg"
+                alt=""
+              />
             </div>
             <div className="relative px-2 py-6">
-              <img className="mw-33p pr300 px350-ns w-100p" height="60" width="60" src="/images/footerLogo/SSIM DELHI.png" />
+              <img
+                className="mw-33p pr300 px350-ns w-100p"
+                height="90"
+                width="90"
+                src="/images/footerLogo/SSIM DELHI.png"
+                alt=""
+              />
             </div>
           </div>
         </div>
---
+++ pages/okay.js
@@ -0,0 +1,10 @@
+const okay = () => {
+  return (
+    <>
+      <img className="w-screen" src="/images/banners/1.png" alt="" />
+      {/* <img src="https://source.unsplash.com/1500x500?grass" alt="" /> */}
+    </>
+  )
+}
+
+export default okay
