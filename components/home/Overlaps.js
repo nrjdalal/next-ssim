a68659dce4a827e8dbe3d6fd5bba543726366e75
Neeraj Dalal <admin@nrjdalal.com>
2021-06-28 18:45:46 +0530

    600px fix

--- components/home/Overlaps.js
+++ components/home/Overlaps.js
@@ -2,7 +2,7 @@ const Overlaps = () => {
   return (
     <>
       <section className="relative text-gray-400 body-font">
-        <div className="h-[512px] sm:h-[224px] xl:h-[96px]"></div>
+        <div className="h-[600px] sm:h-[224px] xl:h-[96px]"></div>
         <div className="absolute top-2 sm:top-[-72px] left-0 px-5 z-10 w-screen">
           <div className="flex flex-wrap sm:mx-auto">
             <div className="p-2 sm:w-1/2 xl:w-1/4 w-full">
