6c7a1847fd69c1bb5336aa192cc821e1202e679f
Neeraj Dalal <admin@nrjdalal.com>
2021-06-28 18:44:06 +0530

    mob adjustment done

--- components/home/Overlaps.js
+++ components/home/Overlaps.js
@@ -3,7 +3,7 @@ const Overlaps = () => {
     <>
       <section className="relative text-gray-400 body-font">
         <div className="h-[512px] sm:h-[224px] xl:h-[96px]"></div>
-        <div className="absolute top-[-72px] left-0 px-5 z-10 w-screen">
+        <div className="absolute top-2 sm:top-[-72px] left-0 px-5 z-10 w-screen">
           <div className="flex flex-wrap sm:mx-auto">
             <div className="p-2 sm:w-1/2 xl:w-1/4 w-full">
               <div className="bg-[#096b97] rounded-3xl flex p-4 h-[128px] items-center justify-center border-4 border-yellow-500">
