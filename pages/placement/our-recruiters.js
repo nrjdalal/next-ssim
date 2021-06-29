5699e5d89f0a7cd1705bab889d45b4e7ae6354e3
abhishek garg <abhishek.garg@namasys.co>
2021-06-29 21:34:22 +0530

    photos

--- pages/placement/our-recruiters.js
+++ pages/placement/our-recruiters.js
@@ -2,24 +2,225 @@ import { Logo150 } from '../../components/Universal'
 import Breadcrumb from '../../components/Breadcrumb'
 
 const rename_later = () => {
-  let srcs = [];
-
-  for (let index = 1; index <= 53; index++) {
-    srcs.push(`/images/recruiters/${index}.jpg`)
-  }
-
 
   return (
     <>
       <Breadcrumb title="Our Recruiters" />
       <div className="py-24">
+        <h3 className="text-center">Automobile</h3>
         <div className="container mx-auto px-6">
           <div className="flex flex-wrap justify-center">
-            {
-              srcs.map((src, index) => {
-                return <Logo150 key={index} src={src} />
-              })
-            }
+            <Logo150 src="/images/recruiters/Autumobile/ (1).jpg" />
+            <Logo150 src="/images/recruiters/Autumobile/ (2).png" />
+            <Logo150 src="/images/recruiters/Autumobile/ (3).png" />
+            <Logo150 src="/images/recruiters/Autumobile/ (4).png" />
+            <Logo150 src="/images/recruiters/Autumobile/ (5).png" />
+            <Logo150 src="/images/recruiters/Autumobile/ (6).png" />
+            <Logo150 src="/images/recruiters/Autumobile/ (7).png" />
+            <Logo150 src="/images/recruiters/Autumobile/ (8).png" />
+            <Logo150 src="/images/recruiters/Autumobile/ (9).png" />
+            <Logo150 src="/images/recruiters/Autumobile/ (10).png" />
+            <Logo150 src="/images/recruiters/Autumobile/ (11).jpg" />
+            <Logo150 src="/images/recruiters/Autumobile/ (12).jpg" />
+            <Logo150 src="/images/recruiters/Autumobile/ (13).png" />
+            <Logo150 src="/images/recruiters/Autumobile/ (14).png" />
+          </div>
+        </div>
+      </div>
+      <div className="py-24">
+        <h3 className="text-center">BFSI (Banking, Financial Service & Insurance)</h3>
+        <div className="container mx-auto px-6">
+          <div className="flex flex-wrap justify-center">
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (1).png" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (2).png" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (3).jpg" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (4).jpg" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (5).jpg" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (6).jpg" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (7).jpg" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (8).png" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (9).jpg" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (10).png" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (11).png" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (12).jpg" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (13).jpg" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (14).png" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (15).png" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (16).png" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (17).png" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (18).png" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (19).jpg" />
+            <Logo150 src="/images/recruiters/BFSI (Banking, Financial Service & Insurance)/ (20).png" />
+          </div>
+        </div>
+      </div>
+      <div className="py-24">
+        <h3 className="text-center">E-Commerce, Retail & Logistics</h3>
+        <div className="container mx-auto px-6">
+          <div className="flex flex-wrap justify-center">
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (1).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (2).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (3).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (4).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (5).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (6).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (7).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (8).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (9).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (10).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/(11).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (12).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (13).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (14).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (15).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (16).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (17).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (18).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (19).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (20).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (21).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (22).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (23).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (24).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (25).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (26).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (27).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (28).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (29).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (30).jpg" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (31).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (32).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (33).png" />
+            <Logo150 src="/images/recruiters/E-Commerce, Retail & Logistics/ (34).jpg" />
+          </div>
+        </div>
+      </div>
+      <div className="py-24">
+        <h3 className="text-center">Ed Tech</h3>
+        <div className="container mx-auto px-6">
+          <div className="flex flex-wrap justify-center">
+            <Logo150 src="/images/recruiters/Ed Tech/ (1).png" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (2).png" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (3).jpg" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (4).png" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (5).jpg" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (6).png" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (7).png" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (8).png" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (9).jpg" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (10).png" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (11).png" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (12).jpg" />
+            <Logo150 src="/images/recruiters/Ed Tech/ (13).png" />
+          </div>
+        </div>
+      </div>
+      <div className="py-24">
+        <h3 className="text-center">FMCG</h3>
+        <div className="container mx-auto px-6">
+          <div className="flex flex-wrap justify-center">
+            <Logo150 src="/images/recruiters/FMCG/ (1).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (2).jpg" />
+            <Logo150 src="/images/recruiters/FMCG/ (3).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (4).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (5).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (6).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (7).jpg" />
+            <Logo150 src="/images/recruiters/FMCG/(8).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (9).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (10).png" />
+            <Logo150 src="/images/recruiters/FMCG/(11).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (12).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (13).jpg" />
+            <Logo150 src="/images/recruiters/FMCG/ (14).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (15).jpg" />
+            <Logo150 src="/images/recruiters/FMCG/(16).png" />
+            <Logo150 src="/images/recruiters/FMCG/ (17).jpg" />
+          </div>
+        </div>
+      </div>
+      <div className="py-24">
+        <h3 className="text-center">Food &amp; Beverages</h3>
+        <div className="container mx-auto px-6">
+          <div className="flex flex-wrap justify-center">
+            <Logo150 src="/images/recruiters/Food Beverages/ (1).jpg" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (2).jpg" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (3).png" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (4).png" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (5).jpg" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (6).png" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (7).png" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (8).jpg" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (9).png" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (10).png" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (11).png" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (12).png" />
+            <Logo150 src="/images/recruiters/Food Beverages/ (13).png" />
+          </div>
+        </div>
+      </div>
+      <div className="py-24">
+        <h3 className="text-center">IT, ITES & Telecom</h3>
+        <div className="container mx-auto px-6">
+          <div className="flex flex-wrap justify-center">
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (1).jpg" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (2).jpg" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (3).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (4).jpg" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (5).jpg" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (6).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (7).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (8).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (9).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (10).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (11).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (12).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (13).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (14).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (15).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (16).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (17).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (18).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (19).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (20).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (21).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (22).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (23).jpg" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (24).png" />
+            <Logo150 src="/images/recruiters/IT, ITES & Telecom/ (25).png" />
+          </div>
+        </div>
+      </div>
+      <div className="py-24">
+        <h3 className="text-center">Other Recruiters</h3>
+        <div className="container mx-auto px-6">
+          <div className="flex flex-wrap justify-center">
+            <Logo150 src="/images/recruiters/Other Recruiters/ (1).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/(2).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/(3).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (4).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (5).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (6).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (7).jpg" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (8).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (9).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (10).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (11).jpg" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (12).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (13).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (14).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (15).jpg" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (16).jpg" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (17).jpg" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (18).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (19).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (20).jpg" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (21).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (22).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (23).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (24).jpg" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (25).png" />
+            <Logo150 src="/images/recruiters/Other Recruiters/ (26).png" />
           </div>
         </div>
       </div>
