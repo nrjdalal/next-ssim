ff7172276029267e183937f142c07605a618a620
nrjdalal <nrjdalal@nrjdalal.local>
2022-01-03 19:11:52 +0530

    page added

---
+++ pages/pgdm/pgdm-pro.js
@@ -0,0 +1,228 @@
+import Breadcrumb from '../../components/Breadcrumb'
+import {
+  ContentOnLeft,
+  ContentOnRight,
+  FlexColumnCenter,
+  ProseWide,
+} from '../../components/Universal'
+
+const the_institute = () => {
+  return (
+    <>
+      <Breadcrumb title="PGDM PRO" />
+      <ProseWide className="py-12">
+        <FlexColumnCenter>
+          <img
+            className="rounded-3xl h-[400px] w-full max-w-[600px] object-cover object-center"
+            src="https://source.unsplash.com/400x300/?corporate"
+            alt=""
+          />
+
+          <h1>120+ Days Live Corporate Exposure</h1>
+          <blockquote>
+            This Entire Program provides hands on experience to the Budding
+            Managers in such a manner that once they are exposed to take up any
+            corporate challenge, they are in a situation from where they can
+            easily encounter and overcome that hurdles or barrier.
+            <br />
+            <br />
+            Our Budding Managers are skilled, trained and developed with the
+            help of giving them an opportunity to participate in Live Projects,
+            Winter Internship, Summer Training Program & International live
+            Projects.
+            <br />
+            <br />
+            Eventually by doing all this our Budding Managers get ready to serve
+            the Corporate World.
+            <br />
+            <br />
+            <ul>
+              <li>3 Live Projects (Duration: 10 days each)</li>
+              <li>1 International Live Project (Duration: 3 Days)</li>
+              <li>1 Winter Internship (Duration: 30 Days)</li>
+              <li>1 Summer Internship (Duration: 60 Days)</li>
+            </ul>
+          </blockquote>
+        </FlexColumnCenter>
+      </ProseWide>
+
+      <ProseWide>
+        <h1 className="pt-12 italic text-center">
+          6 Pro “Skill Enhancement Certificates”
+        </h1>
+      </ProseWide>
+
+      <div className="px-6 pb-8 mx-auto md:w-5/6">
+        <ContentOnRight
+          imgClassName="object-right"
+          pClassName="text-sm xl:text-base"
+          className="pt-24"
+          title="ERP Training "
+          src="/images/corporate_edge/"
+        >
+          NSE course on personal finance teach school students the value of
+          investing, provide an introduction to the Indian capital markets and
+          help to develop new market professionals. Our NSE workshop in
+          Financial Markets, is a program that tests the practical knowledge and
+          skills required to operate in the financial markets.
+        </ContentOnRight>
+
+        <ContentOnLeft
+          pClassName="text-sm xl:text-base"
+          title="Digital Marketing"
+          src="/images/corporate_edge/"
+        >
+          There are several manners in which a person can advantage from getting
+          marketing knowhow, and it’s one of the major perk for attending a
+          workshop in Digital Marketing. Digital Marketing certification cover
+          the skills which are fundamental to the globe of business. These
+          skills include how to convey the viably via written communication
+          means or oral, how to give presentations in an executive events and
+          pitch thoughts. What is more, members find out how to analyze info,
+          shopper requirements and marketing patterns.
+        </ContentOnLeft>
+
+        <ContentOnRight
+          imgClassName="object-left"
+          pClassName="text-sm xl:text-base"
+          title="PGL (Personal Grooming Lab)"
+          src="/images/corporate_edge/"
+        >
+          Personal Grooming Lab has an understated but powerful influence on
+          one’s progress. It helps to build a lasting image of an individual
+          based solely on appearance which gives out a message of
+          professionalism, sophistication, credibility etc. Many do feel that a
+          person should not be defined by looks alone, however one responds
+          positively to someone who is well kempt as opposed to someone who is
+          shabby. Since looks are a visual medium, not just clothes and make up
+          but healthy skin and hair, clipped nails along with good smell send
+          out a subconscious signal about the person.
+        </ContentOnRight>
+
+        <ContentOnLeft
+          pClassName="text-sm xl:text-base"
+          title="Six Sigma "
+          src="/images/corporate_edge/"
+        >
+          Six Sigma is a set of methods and tools for process improvement.
+          Developed in 1986, Six Sigma has become a global phenomenon with
+          companies around the world. Companies and individuals are using six
+          sigma methodologies to improve operational efficiencies. Six Sigma
+          Certification is a great way to improve your capabilities as a leader
+          in your organization. It helps in renovating the business processes.
+        </ContentOnLeft>
+
+        <ContentOnRight
+          pClassName="text-sm xl:text-base"
+          title="Budgeting and Negotiating Skills"
+          src="/images/corporate_edge/"
+        >
+          This Certification helps in explaining what your business objectives
+          are, and how each Expenditure item will help you to realize them. Your
+          Budget may be better received if you can show that you expenditure is
+          in Line you’re your organization’s overall strategic objectives.
+        </ContentOnRight>
+
+        <ContentOnLeft
+          pClassName="text-sm xl:text-base"
+          title="Work Force Management"
+          src="/images/corporate_edge/"
+        >
+          It is an integrated set of Processes that a Company uses to optimize
+          the Productivity of its Employees. WFM involves effectively
+          forecasting Employee requirements and creating and managing staff
+          schedules to accomplish a particular task on day to day basis. This
+          Certification helps the Managers to assess and Plan their Manpower
+          requirement in advance.
+        </ContentOnLeft>
+
+        <div className="h-12" />
+      </div>
+
+      <ProseWide>
+        <h1 className="pt-12 italic text-center">
+          6 Pro “Industry Ready Workshop”
+        </h1>
+      </ProseWide>
+
+      <div className="px-6 pb-8 mx-auto md:w-5/6">
+        <ContentOnRight
+          imgClassName="object-right"
+          pClassName="text-sm xl:text-base"
+          className="pt-24"
+          title="National Stock Exchange (NSE Workshop)"
+          src="/images/corporate_edge/"
+        >
+          NSE course on personal finance teach school students the value of
+          investing, provide an introduction to the Indian capital markets and
+          help to develop new market professionals. Our NSE workshop in
+          Financial Markets, is a program that tests the practical knowledge and
+          skills required to operate in the financial markets.
+        </ContentOnRight>
+
+        <ContentOnLeft
+          pClassName="text-sm xl:text-base"
+          title="SEBI"
+          src="/images/corporate_edge/"
+        >
+          The scope of this workshop lies within a few questions, as to why you
+          should invest, what should be your investment objectives, investment
+          options, the do’s and don’ts of investing, grievance and redressal.
+        </ContentOnLeft>
+
+        <ContentOnRight
+          imgClassName="object-left"
+          pClassName="text-sm xl:text-base"
+          title="MIS (Management Information System)"
+          src="/images/corporate_edge/"
+        >
+          The ultimate goal of the use of a Management Information System is to
+          increase the value and Profits of the business. This is done by
+          providing Managers with timely and appropriate information allowing
+          them to make effective decision with a shorter period of time. This
+          Certification helps the Managers in taking the rational decision for
+          their organizations.
+        </ContentOnRight>
+
+        <ContentOnLeft
+          pClassName="text-sm xl:text-base"
+          title="Retail Management Workshop"
+          src="/images/corporate_edge/"
+        >
+          Retail Management includes all the steps required to bring the
+          customers into the store and fulfill their buying needs. Retail
+          Management makes shopping a pleasurable experience and ensures the
+          customer leave the store with a smile. In short Retail Management
+          helps customers shop without any difficulty. This Certification helps
+          the Managers to better manage the store and avoid big queues for the
+          customers.
+        </ContentOnLeft>
+
+        <ContentOnRight
+          pClassName="text-sm xl:text-base"
+          title="PDP (Personality Development Program)
+          IRDAI Workshop"
+          src="/images/corporate_edge/"
+        ></ContentOnRight>
+
+        <ContentOnLeft
+          pClassName="text-sm xl:text-base"
+          title="Winter Internship Project"
+          src="/images/corporate_edge/"
+        >
+          Winter Internship Project aims to provide Practical Exposure to
+          Budding Managers. This Program ranges from 2-4 weeks. In this Program
+          Students choose the Industry as per their choice and learn the skills
+          which are in demand in the Contemporary World. Students are trained
+          specifically in line with their specializations through which they
+          better understand their Lacunas and Shortcomings so they could improve
+          and enhance their skills in order to better serve the Corporate World.
+        </ContentOnLeft>
+
+        <div className="h-12" />
+      </div>
+    </>
+  )
+}
+
+export default the_institute
